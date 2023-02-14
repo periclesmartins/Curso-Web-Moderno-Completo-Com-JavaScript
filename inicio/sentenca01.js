e7c8f4ad986f9cbe1701f95267123266f83ce0ad
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-02-14 11:21:08 -0300

    sentença de código

---
+++ inicio/sentenca01.js
@@ -0,0 +1,12 @@
+//sentença de código;
+
+console.log('Olá mundo!')
+
+console
+.log('Vamos programar!')
+
+console
+.log
+('Rumo a o primeiro emprego!')
+
+//note que mesmo quebrando linha, os códigos funcionam em javascript;
