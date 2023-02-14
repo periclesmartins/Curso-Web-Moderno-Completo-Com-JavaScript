d37233904203c07b35025e7859173b4d0e6dcd13
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-02-14 15:45:59 -0300

    Update desafio_area.js

--- inicio/desafio_area.js
+++ inicio/desafio_area.js
@@ -1,7 +1,6 @@
 // PI * raio * raio
 const PI = 3.14
 const raio = 10
-const area = 314
+const areaCircunferencia = 314
 
-console.log(`O raio 10 ao quadrado é: ${PI * raio * raio}.`)
-console.log(`O resultado da área é: 314m²`)
+console.log(`O valor da área é: ${areaCircunferencia}m²`)
