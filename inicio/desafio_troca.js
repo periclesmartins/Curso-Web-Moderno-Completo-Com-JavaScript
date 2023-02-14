eb3bec56ac2d878d6b39e5279a8895c432ed1014
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-02-14 16:07:50 -0300

    Trocando valor de vaiáveis!

---
+++ inicio/desafio_troca.js
@@ -0,0 +1,17 @@
+//Desafio: trocar o valor das variáveis!
+//Depois da troca... a = 94 e b = 7!
+
+let a = 7;
+let b = 94;
+
+[a, b] = [b, a]; //troca o valor das variáveis. 
+
+console.log(`a vale: ${a}`);
+console.log(`b vale: ${b}`);
+
+
+//a = b
+//b = temp
+
+//a = 94
+//b = 7
