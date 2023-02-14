842bb30c413085f5330242a729ca6aeb57817a31
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-02-14 11:30:36 -0300

    Create comentario.js

---
+++ inicio/comentario.js
@@ -0,0 +1,12 @@
+// console.log é usado para exbir algo na tela do computador!
+/*
+Molho de pimenta.
+fone de ouvido
+computador
+notebook
+celular
+teclado
+mesa
+caixa de pizza
+*/
+//Essas são as duas maneiras de comentar em javascript, os comentários não afetam o código.
