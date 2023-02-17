62077c82b9ae80784cac8578ca49ef5052e3f7d6
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-02-17 20:40:09 -0300

    Update math.js

--- fundamentos/math.js
+++ fundamentos/math.js
@@ -1,5 +1,11 @@
-const raio = 5.6
-const area = Math.PI * Math.pow(raio, 2)
+// Define uma constante chamada 'raio' e atribui o valor 5.6 a ela
+const raio = 5.6;
 
-console.log(area)
-console.log(typeof Math)
+// Define uma constante chamada 'area' e atribui a ela o valor calculado a partir da fórmula A = π * r^2
+const area = Math.PI * Math.pow(raio, 2);
+
+// Imprime o valor da constante 'area' no console
+console.log(area);
+
+// Imprime o tipo de dado da propriedade 'Math' no console
+console.log(typeof Math);
