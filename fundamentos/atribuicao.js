62d95ce1b016af9fa48ddb39b589f6c6d2cc754f
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-02-17 20:53:44 -0300

    Create atribuicao.js

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,17 @@
+const a = 7 // declara uma variável 'a' e atribui o valor 7 a ela
+let b = 3 // declara uma variável 'b' e atribui o valor 3 a ela
+
+b += a // adiciona o valor de 'a' ao valor atual de 'b' e atribui o resultado a 'b'
+console.log(b) // imprime o valor de 'b' no console (10)
+
+b -= 2 // subtrai 2 do valor atual de 'b' e atribui o resultado a 'b'
+console.log(b) // imprime o valor de 'b' no console (8)
+
+b *= 2 // multiplica o valor atual de 'b' por 2 e atribui o resultado a 'b'
+console.log(b) // imprime o valor de 'b' no console (16)
+
+b /= 2 // divide o valor atual de 'b' por 2 e atribui o resultado a 'b'
+console.log(b) // imprime o valor de 'b' no console (8)
+
+b %= 2 // calcula o resto da divisão do valor atual de 'b' por 2 e atribui o resultado a 'b'
+console.log(b) // imprime o valor de 'b' no console (0)
