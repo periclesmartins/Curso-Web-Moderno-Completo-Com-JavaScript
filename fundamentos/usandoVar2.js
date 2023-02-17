12a4f7d4f45ce0aca50d113ba41d1c57c1c177fd
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-02-17 16:41:28 -0300

    Create usandoVar2.js

---
+++ fundamentos/usandoVar2.js
@@ -0,0 +1,8 @@
+var numero = 1
+{
+    var numero = 2
+    console.log(`dentro = ${numero}`)
+}
+console.log(`fora = ${numero}`)
+
+//Só existe escopo de 'var' dentro de função, não existe dentro de qualquer tipo de bloco
