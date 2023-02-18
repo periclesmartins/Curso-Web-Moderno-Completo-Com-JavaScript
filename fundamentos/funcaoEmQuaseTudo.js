294d4fb996b0ce92640c705d8a1110a116ae4967
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-02-18 13:32:21 -0300

    Update funcaoEmQuaseTudo.js

--- fundamentos/funcaoEmQuaseTudo.js
+++ fundamentos/funcaoEmQuaseTudo.js
@@ -4,3 +4,11 @@ class Produto {}
 console.log(typeof Produto)
 
 //objeto é uma função, e qualquer classe criada em javascript, também é uma função.
+
+/*
+O código tem duas partes:
+
+console.log(typeof Object): Aqui, estamos usando o operador typeof para descobrir o tipo de dados do objeto Object. O objeto Object é um objeto embutido em JavaScript que serve como base para todos os objetos em JavaScript. Nesse caso, a saída será function, pois em JavaScript, uma classe é uma função.
+
+class Produto {} e console.log(typeof Produto): Aqui, estamos criando uma classe chamada Produto. Em seguida, usamos novamente o operador typeof para descobrir o tipo de dados da classe Produto. Como mencionado anteriormente, em JavaScript, uma classe é uma função, e a saída deste comando será function.
+*/
