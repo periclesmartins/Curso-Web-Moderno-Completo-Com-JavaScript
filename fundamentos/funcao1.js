8e6275bb0bd134fa4b0bed4f8c6af7a03e8e10e8
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-02-18 13:24:23 -0300

    Update funcao1.js

--- fundamentos/funcao1.js
+++ fundamentos/funcao1.js
@@ -1,16 +1,14 @@
 // Função sem retorno;
-
 function imprimirSoma(a, b) {
-    console.log(a * b)
+    console.log(a * b) // Imprime a multiplicação de a e b no console.
 }
-imprimirSoma(30, 10) // Ao reescrever a função, ele executa o que pede para ser imprimido dentro do console.
+imprimirSoma(30, 10) // Chama a função imprimirSoma e passa os valores 30 e 10 como argumentos para a e b, respectivamente.
 
 // Função com retorno;
-
-function soma(a, b = 0) {
-    return a + b
+function soma(a, b = 0) { // Define uma função chamada soma, que recebe dois parâmetros, onde o segundo é opcional e assume o valor padrão 0 se não for informado.
+    return a + b // Retorna a soma de a e b.
 }
 
-console.log(soma(20, 20))
-console.log(soma(2))
-console.log(soma())
+console.log(soma(20, 20)) // Chama a função soma e passa os valores 20 e 20 como argumentos para a e b, respectivamente, e imprime o resultado da soma no console.
+console.log(soma(2)) // Chama a função soma e passa o valor 2 como argumento para a, e utiliza o valor padrão 0 para b, e imprime o resultado da soma no console.
+console.log(soma()) // Chama a função soma sem passar argumentos, e utiliza o valor padrão 0 para ambos os parâmetros, e imprime o resultado da soma no console.
