470841e98129d48ae83541acbe3fcef5fd1b835a
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-02-18 13:39:09 -0300

    Update math.js

--- fundamentos/math.js
+++ fundamentos/math.js
@@ -9,3 +9,17 @@ console.log(area);
 
 // Imprime o tipo de dado da propriedade 'Math' no console
 console.log(typeof Math);
+
+
+/*
+O objeto Math é uma biblioteca padrão embutida no JavaScript que fornece várias funções e constantes matemáticas pré-definidas que podem ser usadas em operações matemáticas. Alguns exemplos de funções matemáticas disponíveis no objeto Math incluem:
+
+Math.abs(): retorna o valor absoluto de um número (isto é, o número sem sinal)
+Math.sqrt(): retorna a raiz quadrada de um número
+Math.pow(base, expoente): retorna a base elevada ao expoente especificado
+Math.floor(): arredonda um número para o inteiro mais próximo em direção ao menor valor absoluto
+Math.ceil(): arredonda um número para o inteiro mais próximo em direção ao maior valor absoluto
+Math.round(): arredonda um número para o inteiro mais próximo
+Math.random(): retorna um número aleatório entre 0 (inclusive) e 1 (exclusivo)
+Há várias outras funções e constantes disponíveis no objeto Math que podem ser usadas em cálculos matemáticos no JavaScript.
+*/
