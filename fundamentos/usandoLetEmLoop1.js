d506366587bd747e4bb6474692c00b787f2de5e0
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-02-20 22:32:00 -0300

    Update usandoLetEmLoop1.js

--- fundamentos/usandoLetEmLoop1.js
+++ fundamentos/usandoLetEmLoop1.js
@@ -1,4 +1,7 @@
-for (let index = 0; index < 10; index++) {
-    console.log(index);   
+// loop for, inicia a variável index em 0, executa enquanto index <= 10 e incrementa index a cada iteração
+for (let index = 0; index <= 10; index++) {
+    console.log(index);   // imprime o valor de index a cada iteração
 }
-console.log(`index = ${index}`)
+
+console.log(`index = ${index}`) // tenta imprimir o valor de index, porém, como a variável index foi definida com let, ela só existe dentro do escopo do loop e não está acessível aqui, gerando um ReferenceError
+
