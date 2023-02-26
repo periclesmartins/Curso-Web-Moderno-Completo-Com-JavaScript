a71f838e5774e51077b26c698cb80274715dd62e
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-02-26 20:56:39 -0300

    comentei os códigos

--- fundamentos/logicos.js
+++ fundamentos/logicos.js
@@ -1,14 +1,20 @@
+// função que retorna um objeto com informações de compras baseadas nos parâmetros de entrada
 function compras(trabalho1, trabalho2) {
+    // compra sorvete se pelo menos um dos trabalhos for verdadeiro
     const comprarSorvete = trabalho1 || trabalho2
+    // compra TV 50 polegadas somente se ambos os trabalhos forem verdadeiros
     const comprarTv50 = trabalho1 && trabalho2
-    // const comprarTv32 = !!(trabalho1 ^ trabalho2) //bitwise xor
+    // compra TV 32 polegadas somente se um dos trabalhos for verdadeiro (diferente)
     const comprarTv32 = trabalho1 != trabalho2
+    // mantém a saúde caso não compre sorvete (se comprarSorvete for falso)
     const manterSaudavel = !comprarSorvete //operador unário
 
+    // retorna objeto com informações de compras
     return { comprarSorvete, comprarTv50, comprarTv32, manterSaudavel}
 }
 
-console.log(compras(true, true))
-console.log(compras(true, false))
-console.log(compras(false, true))
-console.log(compras(false, false))
+// testes da função compras com diferentes valores de entrada
+console.log(compras(true, true)) // retorna { comprarSorvete: true, comprarTv50: true, comprarTv32: false, manterSaudavel: false }
+console.log(compras(true, false)) // retorna { comprarSorvete: true, comprarTv50: false, comprarTv32: true, manterSaudavel: false }
+console.log(compras(false, true)) // retorna { comprarSorvete: true, comprarTv50: false, comprarTv32: true, manterSaudavel: false }
+console.log(compras(false, false)) // retorna { comprarSorvete: false, comprarTv50: false, comprarTv32: false, manterSaudavel: true }
--- fundamentos/unarios.js
+++ fundamentos/unarios.js
@@ -1,10 +1,21 @@
+// Declaração das variáveis num1 e num2
 let num1 = 1
 let num2 = 2
 
+// Incremento da variável num1
 num1++
-console.log(num1)
---num1
+
+// Exibe o valor de num1 após o incremento
 console.log(num1)
 
+// Decremento da variável num1 com a pré-decrementação
+--num1
+
+// Exibe o valor de num1 após o decremento com a pré-decrementação
+console.log(num1)
+
+// Incremento da variável num1 com pré-incrementação e comparação do valor com o pós-decremento de num2
 console.log(++num1 === num2--)
+
+// Compara o valor atual de num1 com o valor atual de num2
 console.log(num1 === num2)
