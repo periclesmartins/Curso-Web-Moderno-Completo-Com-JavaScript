fb1c26d23ef15d403121fefa3b776df8450002d3
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-02-26 20:47:50 -0300

    Update aritmeticos.js

--- fundamentos/aritmeticos.js
+++ fundamentos/aritmeticos.js
@@ -1,9 +1,9 @@
-const [a,b,c,d] = [3,5,1,15]
+const [a,b,c,d] = [3,5,1,15] //criação de um array com quatro variáveis e atribuição de valores a cada uma delas
 
-const soma = a + b + c + d 
-const subtracao = d - b
-const multiplicacao = a * b
-const divisao = d / a
-const modulo = a % 2
+const soma = a + b + c + d //soma dos valores de todas as variáveis
+const subtracao = d - b //subtração dos valores de d e b
+const multiplicacao = a * b //multiplicação dos valores de a e b
+const divisao = d / a //divisão do valor de d pelo valor de a
+const modulo = a % 2 //resto da divisão do valor de a por 2
 
-console.log(soma,subtracao,multiplicacao,divisao,modulo)
+console.log(soma,subtracao,multiplicacao,divisao,modulo) //imprime no console os resultados das operações realizadas acima
