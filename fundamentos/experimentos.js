8ae37e723926d2ac5bd04fee75b3b372a69b4865
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-02-26 20:59:23 -0300

    Update experimentos.js

--- fundamentos/experimentos.js
+++ fundamentos/experimentos.js
@@ -1,20 +1,29 @@
+// Declaração da variável a
 let a = 3
 
+// Declaração da variável b com escopo global
 global.b = 123
 
+// Declaração das variáveis c, d e e com o objeto this
 this.c = 456
 this.d = false
 this.e = 'teste'
 
+// Exibe os valores das variáveis a, b, c e o valor de c a partir do objeto module.exports
 console.log(a)
 console.log(global.b)
 console.log(this.c)
 console.log(module.exports.c)
+
+// Verifica se o objeto module.exports é estritamente igual ao objeto this
 console.log(module.exports === this)
+
+// Exibe o conteúdo do objeto module.exports
 console.log(module.exports)
 
-//criando uma variavel maluca; sem var e let!
-abc = 3 // não faça isso!
+// Criação de uma variável sem declarar com var ou let, o que pode causar erros de escopo
+abc = 3
 console.log(global.abc)
 
+// Atribuição de um objeto com as propriedades e, f e g ao objeto module.exports
 // module.exports = { e: 456, f: false, g: 'teste' }
